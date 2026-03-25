1e12550a627df69206a7974ca43b10009db4567a
judgelight <luzhihuavex@gmail.com>
2026-03-25 19:37:59 +0900

    next 更新

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  eslint: {
-    // 构建时忽略ESLint错误，不会导致构建失败
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 's.ar8.top',
-        pathname: '**',
+        protocol: "https",
+        hostname: "s.ar8.top",
+        pathname: "**",
       },
     ],
   },
